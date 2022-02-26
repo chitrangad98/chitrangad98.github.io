5ad188e9195bdd070dcbb74be95c5fd702b526a7
Chitrangad Singh <37699026+chitrangad98@users.noreply.github.com>
2022-02-25 17:17:36 -0700

    dummy change

--- src/data/aboutData.js
+++ src/data/aboutData.js
@@ -1,7 +1,7 @@
 export const aboutData = {
   title: "Who I am",
   description1:
-    "I am currently pursuing my masters in Information Technology and Product Management from Arizona State University. ",
+    "I am currently pursuing my masters in Information Technology and Product Management from Arizona State University.  ",
   description2:
     "I also work on freelance projects and utilize my spare time in building my own applications. At the moment, I'm in pursuit of becoming a full-stack web developer and am seeking internship opportunities in summer 2022. ",
   image: 2,
